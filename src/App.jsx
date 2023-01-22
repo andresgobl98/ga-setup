aa90ff6e4c613e80cc995b3f6e9181e3740a9fb0
Andres Gonzalez <andresfelipegonzalez@hotmail.com>
2023-01-21 22:22:06 -0500

    Changed logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,36 @@ import { useEffect, useState } from 'react';
 import useExperiment from './utils/experiments';
 
 function App() {
-  const [ variantShown, setVariantShown ] = useState(1)
+  const [ variantShown, setVariantShown ] = useState(null)
   const expResult = useExperiment('KGpFV4A0TFaVmdZLKAUzSA');
 
   useEffect(() => {
     setVariantShown(expResult)
   }, [expResult])
 
-  console.log(variantShown)
-  
+  const showExperiment = () => {
+    switch (variantShown) {
+      case 0: 
+        return (
+          <div className={'experiment experiment-1'}>
+            <span>This is the ORIGINAL</span>
+          </div>
+        )
+      case 1:
+        return (
+          <div className={'experiment experiment-2'}>
+            <span>This is the VARIANT</span>
+          </div>
+        )
+      default:
+        return (
+          <div className={'experiment experiment-1'}>
+            <span>This is the ORIGINAL</span>
+          </div>
+        )
+    }
+  }
+
   return (
     <div className="App">
       <div className='content'>
@@ -25,15 +46,7 @@ function App() {
           }}>
           Change Variant
         </button> */}
-        {variantShown === 1 ?
-          <div className={'experiment experiment-2'}>
-            <span>This is the VARIANT</span>
-          </div>
-          :
-          <div className={'experiment experiment-1'}>
-            <span>This is the ORIGINAL</span>
-          </div>
-        }
+        {showExperiment()}
       </div>
     </div>
   );
