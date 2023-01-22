8745b663377201ea11b88805b5f4e40b1bf2c559
Andres Gonzalez <andresfelipegonzalez@hotmail.com>
2023-01-22 00:48:20 -0500

    Changed all to Growthbook

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,58 @@
 import './App.scss';
 import { useEffect, useState } from 'react';
-import useExperiment from './utils/experiments';
+import { nanoid } from "nanoid";
+import { GrowthBook, GrowthBookProvider, useFeature } from "@growthbook/growthbook-react";
+
+const FEATURES_ENDPOINT = "https://cdn.growthbook.io/api/features/prod_2oJkcRE2G1vC1e8D0jXo9akFWxFO36NZm2i3i0qTZ90";
 
 function App() {
-  const [ variantShown, setVariantShown ] = useState(null)
-  const expResult = useExperiment('KGpFV4A0TFaVmdZLKAUzSA');
+  const [visitorId, setVisitorId] = useState(localStorage.getItem('visitor_id'))
+  const container_type = useFeature('text-container').on
 
   useEffect(() => {
-    setVariantShown(expResult)
-  }, [expResult])
-
-  const showExperiment = () => {
-    switch (variantShown) {
-      case 0: 
-        return (
-          <div className={'experiment experiment-1'}>
-            <span>This is the ORIGINAL</span>
-          </div>
-        )
-      case 1:
-        return (
-          <div className={'experiment experiment-2'}>
-            <span>This is the VARIANT</span>
-          </div>
-        )
-      default:
-        return (
-          <div className={'experiment experiment-1'}>
-            <span>This is the ORIGINAL</span>
-          </div>
-        )
+    if (!visitorId) {
+      const new_visitor_id = nanoid()
+      localStorage.setItem('visitor_id', new_visitor_id)
+      setVisitorId(new_visitor_id)
     }
-  }
+  }, [visitorId])
+
+  // Create a GrowthBook instance
+  const growthbook = new GrowthBook({
+    attributes: {
+      id: visitorId,
+    },
+    trackingCallback: (experiment, result) => {
+      console.log({
+        experiment: experiment,
+        result: result,
+      })
+    },
+  })
+
+  fetch(FEATURES_ENDPOINT)
+    .then((res) => res.json())
+    .then((json) => {
+      growthbook.setFeatures(json.features)
+    })
 
   return (
-    <div className="App">
-      <div className='content'>
-        <h1>Google Analytics playground</h1>
-        {/* <button className='button' onClick={() => {
-          if (variantShown === 1) {
-            setVariantShown(2)
-            return
+    <GrowthBookProvider growthbook={growthbook}>
+      <div className="App">
+        <div className='content'>
+          <h1>Google Analytics playground</h1>
+          {container_type ?
+            <div className={'experiment experiment-2'}>
+              <span>This is the VARIANT</span>
+            </div>
+            :
+            <div className={'experiment experiment-1'}>
+              <span>This is the ORIGINAL</span>
+            </div>
           }
-          setVariantShown(1)
-          }}>
-          Change Variant
-        </button> */}
-        {showExperiment()}
+        </div>
       </div>
-    </div>
+    </GrowthBookProvider>
   );
 }
 
